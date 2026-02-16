cdb353edcc5961e0ec9ef143b6ce5147f18c444a
SushanthReddy <aletisushanthreddy@gmail.com>
2026-02-16 12:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function firstWord(s) {
   // your code here
+	if(!s)return "";
+	s=s.trimStart();
+	const spaceIndex=s.indexOf(" ");
+	return spaceIndex===-1?s:s.slice(0,spaceIndex);
 }
+
 
 // Do not change the code below
 
